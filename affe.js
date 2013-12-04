611be512d73a2f0eb319bfce41aa2c867ec27738
viktorberzsinszky <vberzsin@gmail.com>
2013-12-04 17:35:38 +0100

    kommen sie hirtelen

--- affe.js
+++ affe.js
@@ -3,26 +3,36 @@
  y(t) = v0 *t *sin(a) - (g /2) *t*t;
 */
 
-g = 9.80665;
 
 compute = function(v, a, t)
 {
 	hr = v *t *Math.cos(a /180 *Math.PI);
 	vr = v *t *Math.sin(a /180 *Math.PI) -(g /2 *t *t);
-	console.log("hr:" +parseInt(hr) +":vr:" +parseInt(vr) +":v:" +v);
-	t++;
-	// t+= 1/30;
-	v -= 0.1;
-	if(0 >= v){
-		v = 0;
-	}
-	if(-5 < vr){
+	console.log(
+		"hr:" +hr + "\t"+
+		"vr:" +vr + "\t"+
+		"v: " +v
+	);
+	t += 1/fps;
+	v -= 1/fps;
+	if(0 >= v){ v = 0; }
+	if(vr > -5){
 		compute(v, a, t);
+	}
+	else{
+		console.log("bounce");
+		if(v > 0){
+			t = 0;
+			compute(v, a, t);
+		}
 	}
 }
 
-v = 200;
-a = 45;
+v = 100;
+a = 89;
 t = 0;
+fps = 1;
+
+g = 9.80665 /fps;
 
 compute(v, a, t);
